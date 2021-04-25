c662f1f743da43422a9042f24a7abfc7c61762b8
Garrett <garrettn@gmail.com>
2021-04-25 13:32:55 -0500

    Added a TestNG example.

--- src/test/java/SeleniumWithTestNG.java
+++ src/test/java/SeleniumWithTestNG.java
@@ -1,3 +1,31 @@
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class SeleniumWithTestNG {
 
+    private SeleniumExample seleniumExample;
+    private String expectedTitle = "About Baeldung | Baeldung";
+
+    @BeforeSuite
+    public void setUp() {
+        seleniumExample = new SeleniumExample();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        seleniumExample.closeWindow();
+    }
+
+    @Test
+    public void exampleTest() {
+        seleniumExample.getAboutBaeldungPage();
+        String actualTitle = seleniumExample.getTitle();
+
+        assertNotNull(actualTitle);
+        assertEquals(expectedTitle, actualTitle);
+        assertTrue(seleniumExample.isAuthorInformationAvailable());
+    }
 }
